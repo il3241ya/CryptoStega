8205148ad25e5180357dca1d5bfa6f164b783de5
il3241ya <il3241ya@gmail.com>
2023-11-04 20:44:18 +0300

    Migration from an old repository

---
+++ Vigener/brutforser.py
@@ -0,0 +1,21 @@
+from vizhiner import decrypt
+
+
+def brutforcer():
+	print("Введите закрытый текст:")
+	text = input()
+
+	print("Введите тип шифра:\n 1 - самоключ;\n 2 - самоключ по шифротексту.")
+	type = int(input())
+
+	while type != 1 or type != 2:
+		print("Введите тип шифра:\n 1 - самоключ;\n 2 - самоключ по шифротексту.")
+		type = int(input())
+
+	for key in "qwertyuiopasdfghjklzxcvbnm":
+		open_text = decrypt(text, key, type+1)
+		print(key,'-',open_text[:40])
+
+
+if __name__ == "__main__": 
+	brutforcer()
